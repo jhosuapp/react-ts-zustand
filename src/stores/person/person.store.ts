c60d839711def5c9ce517d358afef48cb91733f0
jhosuapp <jhosuapvll@gmail.com>
2025-02-23 14:51:48 -0500

    feat: persist and statecreator

---
+++ src/stores/person/person.store.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+
+interface PersonState {
+    firstName: string;
+    lastName: string;
+}
+
+interface Actions {
+    setFirstName: (value: string) => void;
+    setLastName: (value: string) => void;
+}
+
+
+export const usePersonStore = create<PersonState & Actions>()(
+    persist(
+        (set) =>({
+            firstName: '',
+            lastName: '',
+            
+            setFirstName: (value: string) => set((state) => ({
+                firstName: value
+            })),
+            setLastName: (value: string) => set((state) => ({
+                lastName: value
+            }))
+        }),
+        {
+            name: 'person-store'
+        }
+    )
+);
