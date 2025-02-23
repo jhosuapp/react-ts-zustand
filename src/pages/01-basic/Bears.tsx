f317e188f5a852efef8384bccab50d98b6d4f768
jhosuapp <jhosuapvll@gmail.com>
2025-02-23 13:48:43 -0500

    feat: computed properties

--- src/pages/01-basic/Bears.tsx
+++ src/pages/01-basic/Bears.tsx
@@ -6,12 +6,16 @@ const Bears = ():JSX.Element => {
 
     const bears = useBearStore(  useShallow( state => state.bears ) );
     const doNothing = useBearStore( state => state.doNothing );
+    const addBear = useBearStore( state => state.addBear );
+    const clearBears = useBearStore( state => state.clearBears );
 
     return (
         <WhiteCard >
           <h2>Osos</h2>
 
-            <button onClick={ doNothing } className="btn my-5">Do nothing</button>
+            <button onClick={ doNothing } className="btn mt-5 mb-2">Hacer nada</button>
+            <button onClick={ addBear } className="btn my-2">Añadir oso</button>
+            <button onClick={ clearBears } className="btn mt-2 mb-5">Eliminar osos</button>
 
             <pre>
                 { JSON.stringify(bears, null, 2) }
--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -19,13 +19,25 @@ interface BearState {
     increasePandaBears: BearIncrease;
     
     doNothing: () => void;
+    addBear: () => void;
+    clearBears: () => void;
+
+    computed: {
+        totalBears: number
+    }
 }
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useBearStore = create<BearState>()((set, get) => ({
     bears: [{ id: 1, name: 'Bear #1' }],
     blackBears: 0,
     polarBears: 5,
     pandaBears: 10,
+
+    computed: {
+        get totalBears(){
+            return get().blackBears + get().polarBears + get().pandaBears + get().bears.length;
+        }
+    },
 
     increaseBlackBears: (by: number) => set(
         (state) => ({
@@ -46,5 +58,15 @@ export const useBearStore = create<BearState>()((set) => ({
         (state) => ({
             bears: [...state.bears]
         })
+    ),
+    addBear: () => set(
+        (state) => ({
+            bears: [...state.bears, { id: state.bears.length + 1, name: `Oso #${state.bears.length + 1}` }]
+        })
+    ),
+    clearBears: () => set(
+        {
+            bears: []
+        }
     )
 }));
